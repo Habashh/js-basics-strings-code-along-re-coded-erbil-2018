3f921d6e4f42aa69dafe88171741b272bb42dcdd
zaid85j <zaid85j@gmail.com>
2019-02-01 20:59:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,3 @@ var welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 var excitedWelcomeMessage = `welcomeMessage ${welcomeMessage.toUpperCase()}!`;
 
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
-
-
-
